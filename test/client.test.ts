447bf9ce052e26c4b3e7c3e085a0a445a2bd9cd8
fuxingZhang <502545703@qq.com>
2020-06-16 19:37:54 +0800

    complete client test file

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,15 +1,30 @@
+import {
+  assert,
+  assertEquals,
+  assertThrowsAsync,
+} from "https://deno.land/std/testing/asserts.ts";
 import { Client } from "../mod.ts";
 
-const client = new Client({
-  password: "esri@123",
-});
+const { test } = Deno;
 
-try {
+test("client should ok", async () => {
+  const client = new Client({
+    password: "esri@123",
+  });
+
   await client.connect();
-  const result = await client.query("select * from test");
-  console.log(result);
-} catch (error) {
-  console.log(error);
-} finally {
-  await client.end();
-}
+
+  try {
+    const res = await client.query("SELECT * from users");
+    assert(res);
+    assert(res.command === "SELECT");
+    assert(typeof res.rowCount === "number");
+    assert(Array.isArray(res.rows));
+    assert(Array.isArray(res.fields));
+  } catch (error) {
+    console.log(error);
+    assert(false);
+  } finally {
+    await client.end();
+  }
+});
